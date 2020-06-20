602a026be7e1b5c39bdc6b025976e74cf7d61d02
impartial-genre-2648 <vgscholz@gmail.com>
2020-06-20 16:34:13 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -23,3 +23,16 @@ puts phrase
 puts phrase
 puts phrase
 
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
